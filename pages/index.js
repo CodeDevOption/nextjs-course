5e79e1588cb462019d340cb75a3bdb36a58064ac
CodeDevOption <codedevoption@gmail.com>
2023-03-05 06:47:23 +0530

    server-side-natigation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -13,6 +16,7 @@ const Home = () => {
         This is the Home Page
         <Link href="/products">Products</Link>
         <Link href="/blogs">Blogs</Link>
+        <button onClick={() => router.push("/docs/introduction")}>Doc</button>
       </main>
     </>
   );
