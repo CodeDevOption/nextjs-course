0eca064bcf6fd54b530d0b26d35b4e79543ee204
CodeDevOption <codedevoption@gmail.com>
2023-03-07 20:37:35 +0530

    redirects-nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: () => {
+    return [{ source: "/posts", destination: "/", permanent: true }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
